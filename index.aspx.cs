02f5f65d0cbce597538dbe503779ab2739d99e40
BurhanGunaydin <burhan@useinsider.com>
2018-03-07 17:28:08 +0300

    new_theme
    Changed web site theme

---
+++ index.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class index : System.Web.UI.Page
+{
+    protected void SendMail()
+    {
+        // Gmail Address from where you send the mail
+        var fromAddress = "burhangunaydinsite@gmail.com";
+        // any address where the email will be sending
+        var toAddress = "burhangunaydinsite@gmail.com";
+        //Password of your gmail address
+        const string fromPassword = "trabzon61";
+        // Passing the values and make a email formate to display
+        string subject = "Siteden Gelen";
+        string body = "From: " + YourName.Text + "\n";
+        body += "Email: " + YourEmail.Text + "\n";
+        body += "Subject: " + "Siteden Gelen" + "\n";
+        body += "Mesaj: \n" + Comments.Text + "\n";
+        // smtp settings
+        var smtp = new System.Net.Mail.SmtpClient();
+        {
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
+            smtp.Timeout = 20000;
+        }
+        // Passing values to smtp object
+        smtp.Send(fromAddress, toAddress, subject, body);
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            //here on button click what will done 
+            SendMail();
+            DisplayMessage.Text = "Your Comments after sending the mail";
+            DisplayMessage.Visible = true;
+            YourEmail.Text = "";
+            YourName.Text = "";
+            Comments.Text = "";
+        }
+        catch (Exception) { }
+    }
+}
